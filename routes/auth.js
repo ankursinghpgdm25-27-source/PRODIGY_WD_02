cbe33552531bdc366056d7a5607b78079bce77bb
ankursinghpgdm25-27-source <ankursinghpgdm25-27@lloydbusinessschool.edu.in>
2025-09-04 16:34:50 +0530

    routes/auth.js

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+// Authentication routes
+const express = require("express");
+const router = express.Router();
+
+// TODO: implement register, login
+router.post("/register", (req, res) => {
+  res.send("Register route");
+});
+
+router.post("/login", (req, res) => {
+  res.send("Login route");
+});
+
+module.exports = router;
